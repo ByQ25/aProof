16f637545110e41d254bba7832e6909ca0f1ffa0
Marcin Paluch <m.paluch.93@protonmail.com>
2022-10-23 21:12:33 +0200

    Temporarily added a simple console interface

--- aProof/AProofMain.cs
+++ aProof/AProofMain.cs
@@ -6,8 +6,22 @@ namespace aProof
 	{
 		static void Main(string[] args)
 		{
+			char key;
 			Environment env = new Environment(2);
-			Console.ReadKey(true);
+			Console.Title = "aProof";
+			Console.WriteLine("The environment with agents has been initialized.\n");
+			Console.WriteLine("Please select an action to execute by pressing corresponding numeric key:");
+			Console.WriteLine("1. Do the thinking only\n2. Carry a conversation between agents\n3. Quit\n");
+			do
+			{
+				key = Console.ReadKey(true).KeyChar;
+				switch (key)
+				{
+					case '1': env.LetAgentsThinkInAdvance(); break;
+					case '2': env.CarryConversation(); break;
+				}
+			} while (key != '3');
+
 		}
 
 		public class AProofException : ApplicationException
