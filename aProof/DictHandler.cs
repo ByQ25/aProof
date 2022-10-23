1c4b120fd5d576dc716dfe4eb27dcb5595c8347a
Marcin Paluch <m.paluch.93@protonmail.com>
2022-10-23 20:33:24 +0200

    Organizing using clauses

--- aProof/DictHandler.cs
+++ aProof/DictHandler.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography;
 
